547f2a9701a77d8af54eeafd4b10af9bff686870
raytrek2-\ktc <>
2026-01-08 18:16:32 +0900

    テスト登録でダンまち21巻を登録

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'inventory',
 ]
 
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',  # APIはJSONのみ返す設定
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    # permission_classes = [IsAuthenticated]  # 後でJWT or Session連携
